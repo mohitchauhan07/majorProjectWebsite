bacdf0ebdc910f7e4b3925d02890b6d69e50aa81
mohitchauhan07 <mohit.99chauhan@gmail.com>
2021-06-18 01:06:06 +0530

    3rd commit

--- src/Major.js
+++ src/Major.js
@@ -5,7 +5,6 @@ import './index.css'
 const Major = () => {
     return(
         <>
-            <h1 style={{color:'var(--headingColor)', fontSize:'100px'}}>THE 3 D RECONSTRUCTION</h1>
             <div className='card'>
 
             </div>
