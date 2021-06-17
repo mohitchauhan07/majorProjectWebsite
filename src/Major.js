906dd690c8f33baf0ca8b8360bcc34a277d9e044
mohitchauhan07 <mohit.99chauhan@gmail.com>
2021-06-17 23:53:45 +0530

    Project Initilization using react

---
+++ src/Major.js
@@ -0,0 +1,20 @@
+
+import React from 'react';
+import './index.css'
+
+const Major = () => {
+    return(
+        <>
+            <h1 style={{color:'var(--headingColor)', fontSize:'100px'}}>THE 3 D RECONSTRUCTION</h1>
+            <h1 style={{color:'var(--paraColor)', fontSize:'100px'}}>Hello i am in mojor</h1>
+            <h1 style={{color:'var(--headingColor)', fontSize:'100px'}}>Hello i am in mojor</h1>
+            <h1 style={{color:'var(--headingColor)', fontSize:'100px'}}>Hello i am in mojor</h1>
+            <div className='card'>
+                
+            </div>
+        </>
+    
+    )
+    ;
+}
+export default Major;
---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Major from './Major';
+
+
+
+ReactDOM.render(
+  <React.StrictMode>
+   <Major />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
