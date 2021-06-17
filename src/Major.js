3dd0300b91bcf72f38e21052dde61793b1d092aa
Pratul <phazela1998@gmail.com>
2021-06-18 00:47:23 +0530

    first commit

--- src/Major.js
+++ src/Major.js
@@ -5,7 +5,7 @@ import './index.css'
 const Major = () => {
     return(
         <>
-            <h1 style={{color:'var(--headingColor)', fontSize:'100px'}}>THE 3 D RECONSTRUCTION</h1>
+            <h1 style={{color:'var(--headingColor)', fontSize:'100px'}}>Pratul</h1>
             <h1 style={{color:'var(--paraColor)', fontSize:'100px'}}>Hello i am in mojor</h1>
             <h1 style={{color:'var(--headingColor)', fontSize:'100px'}}>Hello i am in mojor</h1>
             <h1 style={{color:'var(--headingColor)', fontSize:'100px'}}>Hello i am in mojor</h1>
