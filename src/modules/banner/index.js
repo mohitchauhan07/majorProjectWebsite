e5be3b3f084edf70cd9bd8fd1913caf6b1cf03f5
mohitchauhan07 <mohit.99chauhan@gmail.com>
2021-07-14 17:47:31 +0530

    Banner Section Completed with some changes in technical approach ie margin height width

--- src/modules/banner/index.js
+++ src/modules/banner/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import "./index.css";
+// import VideoPlay from "./video"
+
 
 const Banner = (props) => {
+  
     return (
         <div id="banner-section-1">
             <div class="banner container">
                 <div class="content">
-                    <h2>3D-Constructor's</h2>
-                    <h1>Real Time 3-D Reconstruction of Environment </h1>
+                    <h2>3D Positioning System</h2>
+                    <h1>Real Time 3-D Positioning Based On Neural Network </h1>
                     <p>A novel framework for real-time 3D scene reconstruction 
                         from a video. Using some neural methods that estimate 
                         single-view depth maps separately on each key-frame and
@@ -18,10 +21,20 @@ const Banner = (props) => {
                     </div>
                 </div>
                 <div class="image">
+                {/* <video src='images/view_3.mp4' loop width="750" height="500" > */}
+                    {/* <source src="images/view_3.mp4" type="video/mp4"/> */}
+                {/* </video> */}
 
-                    <video autoplay muted loop id="myVideo">
+                <video autoPlay muted={true} loop>
+                <source src="images/view_3.mp4" type="video/mp4"/>
+                </video>
+                {/* <div onClick={onClickImg}>
+                      <video src={"images/view_3.mp4"} width="600" height="600" loop/>
+                </div> */}
+                    {/* <video autoplay muted loop id="myVideo">
                         <source src="images/view_3.mp4" type="video/mp4"/>
-                    </video>
+                    </video> */}
+                
                 </div>
             </div>
         </div>      
